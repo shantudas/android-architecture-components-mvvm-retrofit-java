e20eaac642371b657ee63b0929f91ed8955be4a9
shantudash <shantudasbd@yahoo.com>
2022-01-17 11:46:38 +0600

    view model init updated

--- app/src/main/java/com/snipex/shantu/androidarchitecturecomponentsmvvmretrofitwithjava/view/ui/Headlines/HeadlinesFragment.java
+++ app/src/main/java/com/snipex/shantu/androidarchitecturecomponentsmvvmretrofitwithjava/view/ui/Headlines/HeadlinesFragment.java
@@ -8,6 +8,7 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -17,6 +18,7 @@ import com.snipex.shantu.androidarchitecturecomponentsmvvmretrofitwithjava.R;
 import com.snipex.shantu.androidarchitecturecomponentsmvvmretrofitwithjava.adapter.HeadlinesAdapter;
 import com.snipex.shantu.androidarchitecturecomponentsmvvmretrofitwithjava.model.Headline;
 import com.snipex.shantu.androidarchitecturecomponentsmvvmretrofitwithjava.response.HeadlinesResponse;
+
 
 import java.util.ArrayList;
 
@@ -74,9 +76,9 @@ public class HeadlinesFragment extends Fragment {
     }
 
     private void initializations(View root) {
-        headlinesViewModel = ViewModelProviders.of(this).get(HeadlinesViewModel.class);
+        headlinesViewModel = new ViewModelProvider(this).get(HeadlinesViewModel.class);
         recyclerViewHeadlines = root.findViewById(R.id.recyclerViewHeadlines);
-        shimmerViewContainerHeadlines=root.findViewById(R.id.shimmerViewContainerHeadlines);
+        shimmerViewContainerHeadlines = root.findViewById(R.id.shimmerViewContainerHeadlines);
         shimmerViewContainerHeadlines.startShimmerAnimation();
     }
 
--- app/src/main/java/com/snipex/shantu/androidarchitecturecomponentsmvvmretrofitwithjava/view/ui/home/HomeFragment.java
+++ app/src/main/java/com/snipex/shantu/androidarchitecturecomponentsmvvmretrofitwithjava/view/ui/home/HomeFragment.java
@@ -9,9 +9,11 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.snipex.shantu.androidarchitecturecomponentsmvvmretrofitwithjava.R;
+import com.snipex.shantu.androidarchitecturecomponentsmvvmretrofitwithjava.view.ui.Headlines.HeadlinesViewModel;
 
 
 public class HomeFragment extends Fragment {
@@ -20,8 +22,7 @@ public class HomeFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
+        homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         final TextView textView = root.findViewById(R.id.text_home);
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
